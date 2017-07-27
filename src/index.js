e5c4e26a8b70c03717248175417e56bc22ddbe14
KoDookie <nepter2526@gmail.com>
2017-07-27 11:17:42 +0900

    example reaction from mobx

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { autorun, observable, action, useStrict } from 'mobx';
+useStrict(true);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+let state = observable({
+    firstName: "Amorndej",
+    lastName: "Wannasri",
+    get fullName() {
+        //computed values
+        return this.firstName + ' ' + this.lastName;
+    },
+    setName: action(function (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    })
+});
+
+//reaction
+autorun(() => {
+    console.log(state.fullName);
+});
+
+state.setName("ko", "dookie");
+
+//class Profile {
+//    @observable firstName;
+//    @observable lastName;
+//    @observable age;
+//}
+
+//window.state = state;
+//state.name = "koko";
+//state.name = "koko dookie";
